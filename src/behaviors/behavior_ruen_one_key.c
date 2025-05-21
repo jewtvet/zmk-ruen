b5037d81ce8c5804769e01a411d7957defe62a99
jewtvet <koala.lite.yt@gmail.com>
2025-05-21 22:22:02 +0400

    macro 10

--- src/behaviors/behavior_ruen_one_key.c
+++ src/behaviors/behavior_ruen_one_key.c
@@ -9,9 +9,9 @@
 #include <zmk/endpoints.h>
 #include <zephyr/kernel.h>
 
-struct ruen_one_key_config {
-    uint32_t to_en;
-    uint32_t to_ru;
+struct behavior_ruen_one_key_config {
+    char *en_behavior_dev;
+    char *ru_behavior_dev;
 };
 
 static int on_ruen_one_key_pressed(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
@@ -29,26 +29,27 @@ static int on_ruen_one_key_released(struct zmk_behavior_binding *binding, struct
         raise_zmk_keycode_state_changed_from_encoded(encoded, false, event.timestamp + 5);
     } else {
         const struct device *dev = zmk_behavior_get_binding(binding->behavior_dev);
-        const struct ruen_one_key_config *config = dev->config;
-        uint32_t encoded1 = need ? config->to_en : config->to_ru;
-        uint32_t encoded2 = need ? config->to_ru : config->to_en;
+        const struct behavior_ruen_one_key_config *cfg = dev->config;
+        struct zmk_behavior_binding macro_binding1 = {.behavior_dev = need ? cfg->en_behavior_dev : cfg->ru_behavior_dev};
+        struct zmk_behavior_binding macro_binding2 = {.behavior_dev = need ? cfg->ru_behavior_dev : cfg->en_behavior_dev};
         zmk_hid_keyboard_clear();
         zmk_endpoints_send_report(HID_USAGE_KEY);
         zmk_ruen_set_eng(need);
-        raise_zmk_keycode_state_changed_from_encoded(encoded1, true, event.timestamp);
-        k_msleep(5);
-        raise_zmk_keycode_state_changed_from_encoded(encoded1, false, event.timestamp + 5);
+        zmk_behavior_invoke_binding(&macro_binding1, event, true);
+        zmk_behavior_invoke_binding(&macro_binding1, event, false);
         k_msleep(wait);
-        raise_zmk_keycode_state_changed_from_encoded(encoded, true, event.timestamp + 5 + wait);
+        event.timestamp += wait;
+        raise_zmk_keycode_state_changed_from_encoded(encoded, true, event.timestamp);
         k_msleep(5);
-        raise_zmk_keycode_state_changed_from_encoded(encoded, false, event.timestamp + 10 + wait);
+        event.timestamp += 5;
+        raise_zmk_keycode_state_changed_from_encoded(encoded, false, event.timestamp);
         k_msleep(5);
+        event.timestamp += 5;
         zmk_hid_keyboard_clear();
         zmk_endpoints_send_report(HID_USAGE_KEY);
-        zmk_ruen_set_eng(!need);
-        raise_zmk_keycode_state_changed_from_encoded(encoded2, true, event.timestamp + 15 + wait);
-        k_msleep(5);
-        raise_zmk_keycode_state_changed_from_encoded(encoded2, false, event.timestamp + 20 + wait);
+        zmk_ruen_set_eng(!need);    
+        zmk_behavior_invoke_binding(&macro_binding2, event, true);
+        zmk_behavior_invoke_binding(&macro_binding2, event, false);
         k_msleep(wait);
     }
     return ZMK_BEHAVIOR_OPAQUE;
@@ -59,11 +60,11 @@ static const struct behavior_driver_api behavior_ruen_one_key_driver_api = {
     .binding_released = on_ruen_one_key_released,
 };
 
-#define RUEN_ONE_KEY_INST(n)                                                     \
-    static const struct ruen_one_key_config ruen_one_key_config_##n = {          \
-        .to_en = DT_INST_PROP(n, to_en),                                         \
-        .to_ru = DT_INST_PROP(n, to_ru),                                         \
-    };                                                                           \
-    BEHAVIOR_DT_INST_DEFINE(n, NULL, NULL, NULL, &ruen_one_key_config_##n, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_ruen_one_key_driver_api);
+#define RUEN_ONE_KEY_INST(n)                                                              \
+    static const struct behavior_ruen_one_key_config behavior_ruen_one_key_config_##n = { \
+        .en_behavior_dev = DEVICE_DT_NAME(DT_INST_PHANDLE_BY_IDX(n, bindings, 0)),        \
+        .ru_behavior_dev = DEVICE_DT_NAME(DT_INST_PHANDLE_BY_IDX(n, bindings, 1)),        \
+    };                                                                                    \
+    BEHAVIOR_DT_INST_DEFINE(n, NULL, NULL, NULL, &behavior_ruen_one_key_config_##n, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_ruen_one_key_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(RUEN_ONE_KEY_INST)
