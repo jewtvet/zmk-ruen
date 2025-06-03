ea0d40a0ba337860c328ead36faa73c317e23351
jewtvet <koala.lite.yt@gmail.com>
2025-06-04 02:44:06 +0400

    macro 13

--- src/behaviors/behavior_ruen_switch.c
+++ src/behaviors/behavior_ruen_switch.c
@@ -21,14 +21,35 @@ static int on_ruen_switch_pressed(struct zmk_behavior_binding *binding, struct z
 static int on_ruen_switch_released(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
     const struct device *dev = zmk_behavior_get_binding(binding->behavior_dev);
     const struct behavior_ruen_switch_config *cfg = dev->config;
-    uint8_t wait = zmk_ruen_get_macos() ? 50 : 5;
+    uint8_t wait = zmk_ruen_get_macos() ? 46 : 1;
     bool is_eng = binding->param1 != 0;
-    struct zmk_behavior_binding macro_binding = {.behavior_dev = is_eng ? cfg->en_behavior_dev : cfg->ru_behavior_dev};
+    // uint32_t code = binding->param2;
     zmk_hid_keyboard_clear();
     zmk_endpoints_send_report(HID_USAGE_KEY);
     zmk_ruen_set_eng(is_eng);
-    zmk_behavior_invoke_binding(&macro_binding, event, true);
-    k_msleep(wait + 10);
+    // raise_zmk_keycode_state_changed_from_encoded(code, true, event.timestamp);
+    // k_msleep(5);
+    // raise_zmk_keycode_state_changed_from_encoded(code, false, event.timestamp + 5);
+    const char *selected_macro = is_eng ? cfg->en_behavior_dev : cfg->ru_behavior_dev;
+    const struct device *macro_dev = zmk_behavior_get_binding(selected_macro);
+    const struct behavior_macro_config *macro_cfg = macro_dev->config;
+    const struct zmk_behavior_binding *bindings = macro_cfg->bindings;
+    int count = macro_cfg->binding_count;
+    const struct device *kp_dev = zmk_behavior_get_binding("kp");
+    for (int i = 0; i < count; i++) {
+        const struct zmk_behavior_binding *b = &bindings[i];
+        const struct device *b_dev = zmk_behavior_get_binding(b->behavior_dev);
+        if (b_dev == kp_dev) {
+            uint32_t keycode = b->param1;
+            raise_zmk_keycode_state_changed_from_encoded(keycode, true, event.timestamp);
+            k_msleep(5);
+            event.timestamp += 5;
+            raise_zmk_keycode_state_changed_from_encoded(keycode, false, event.timestamp);
+            k_msleep(5);
+            event.timestamp += 5;
+        }
+    }
+    k_msleep(wait);
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
