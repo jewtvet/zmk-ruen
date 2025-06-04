0904b0088b59aff4d74aa14fc4730fbe0107d6b5
jewtvet <koala.lite.yt@gmail.com>
2025-06-04 04:26:24 +0400

    macro 14.1

--- src/behaviors/behavior_ruen_switch.c
+++ src/behaviors/behavior_ruen_switch.c
@@ -42,11 +42,10 @@ static int on_ruen_switch_released(struct zmk_behavior_binding *binding, struct 
     const struct behavior_macro_config *macro_cfg = (const struct behavior_macro_config *)macro_dev->config;
     const struct zmk_behavior_binding *bindings = macro_cfg->bindings;
     int count = (int)macro_cfg->count;
-    const struct device *kp_dev = zmk_behavior_get_binding("KP");
+    const char *kp_label = "KP";
     for (int i = 0; i < count; i++) {
         const struct zmk_behavior_binding *b = &bindings[i];
-        const struct device *b_dev = zmk_behavior_get_binding(b->behavior_dev);
-        if (b_dev == kp_dev) {
+        if (strcmp(b->behavior_dev, kp_label) == 0) {
             uint32_t keycode = b->param1;
             raise_zmk_keycode_state_changed_from_encoded(keycode, true, event.timestamp);
             k_msleep(5);
